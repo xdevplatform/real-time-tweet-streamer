e3037d666dfb6f4327a64c3ee2cf6a8713d08be6
Brandon LeBoeuf <54607186+brandonleboeuf@users.noreply.github.com>
2020-11-24 19:28:38 -0800

    Update setupProxy.js
    The destructuring import causes an error. Removing the curly brackets fixes it.
    
    createProxyMiddleware is not a function
    [0] npm ERR! code 1
    [0] npm ERR! path /Users/bleb/DEV/play/TWITTER/real-time-tweet-streamer
    [0] npm ERR! command failed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const createProxyMiddleware = require("http-proxy-middleware");
 
 // This proxy redirects requests to /api endpoints to
 // the Express server running on port 3001.
